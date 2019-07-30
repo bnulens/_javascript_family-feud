8bf7bf614b41bd9ad5f8f655cd61e0f4dc863fad
Brecht <brechtnulens@gmail.com>
2019-07-30 16:59:44 +0200

    tuesday afternoon

--- familyFunction.js
+++ familyFunction.js
@@ -10,55 +10,6 @@ var aboutMe = {
     Single: false
 };
 
-// Get the object to print in HTML
-document.getElementById("personalButton").addEventListener("click", function(){
-    document.getElementById("section-one").innerHTML = "I am " + aboutMe.Name + " and I am " + aboutMe.Age + " years old.";
-});
-
-// Make BUTTONS toggle 
-function houdini () {
-    var x = document.getElementById("section-one");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-}
-function houdini2 () {
-    var y = document.getElementById("section-two");
-    if (y.style.display === "none") {
-      y.style.display = "block";
-    } else {
-      y.style.display = "none";
-    }
-}
-function familyToggle(button) {
-    var x = button.id;
-    switch (x) {
-        case 'personalButton':
-            houdini(x);
-            break;
-        case 'familyButton':
-            houdini2(x);
-            break;
-        case 'familyMembers':
-            houdini(x);
-            break;
-        case 'fatherButton':
-            houdini(x);
-            break;
-        case 'motherButton':
-            houdini(x);
-            break;      
-        case 'coachButton':
-            houdini(x);
-            break;
-        default:
-            return false;
-    }
-}
-
-
 // Section Two 
 // CONTRUCTOR for family
 function Person(first, last, age, eye) {
@@ -70,10 +21,76 @@ function Person(first, last, age, eye) {
 
 var myFather = new Person("Peter", "Nulens", 57, "Brown");
 var myMother = new Person("Guadelupe", "Acedo", 52, "Grey");
-var myCoach = new Person("Shutsen", "", 33, "Brown");
-var familyMembers = document.getElementById("family-members");
+var myCoach = new Person("Shutsen", " ", 33, "Brown");
 
-document.getElementById("familyButton").addEventListener("click", function(){
-    document.getElementById("section-two").innerHTML = familyMembers;
-});
+let personalButton = document.getElementById("personal-button");
+let familyButton = document.getElementById("family-button");
 
+personalButton.addEventListener("click", function () {
+    document.getElementById("screen").innerHTML = "Hi, I am " + aboutMe.Name + " and I am " + aboutMe.Age + " years old.";
+})
+
+familyButton.addEventListener("click", function getFamilyMembers(){
+})
+
+// Make BUTTONS toggle 
+function houdini () {
+    var x = document.getElementById("screen");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function buttonToggle(button) {
+    var x = button.id;
+    switch (x) {
+        case 'personal-button':
+            houdini(x);
+            break;
+        case 'family-button':
+            houdini(x);
+            break;
+        default:
+            return false;
+    }
+}
+
+function getFamilyMembers () {
+var div = document.createElement("div");
+var fatherButton = document.createElement("button");
+var motherButton = document.createElement("button");
+var coachButton = document.createElement("button");
+
+div.setAttribute("class","family-buttons");
+fatherButton.setAttribute("id","father-button");
+motherButton.setAttribute("id","mother-button");
+coachButton.setAttribute("id","coach-button");
+
+div.appendChild(fatherButton);
+div.appendChild(motherButton);
+div.appendChild(coachButton);
+
+return div;
+}
+          
+
+// var btn = document.createElement("BUTTON");   // Create a <button> element
+// btn.innerHTML = "CLICK ME";                   // Insert text
+// document.body.appendChild(btn);               // Append <button> to <body>
+
+
+// All the data has to be displayed under the button
+// "myFamily" has to have multiple buttons
+// I want to make familyButtons on clicking "myFamily"
+// familyButtons have to display IMG & data on clicking each individual 
+// IMG & data have to be toggled on/off 
+
+// - function for toggle 
+
+// - get by id 
+
+// - create button with id ="" + onclick 
+
+// - display in "family-display"
