42509275598bf704c9c8b35b9ee2a367ddf41765
Brecht <brechtnulens@gmail.com>
2019-07-30 22:09:53 +0200

    tuesday night

--- familyFunction.js
+++ familyFunction.js
@@ -1,13 +1,13 @@
 // Section One 
 // create "aboutMe" object 
 var aboutMe = {
-    Name: "Brecht",
-    Age: 23,
-    Gender: "Male",
-    "Favorite food": "Chicken Curry",
-    "Favorite series": ["Chernobyl","Mad Men", "Black Mirror"],
-    "Favorite movies": ["Tora Tora Tora", "Black Cat, White Cat"],
-    Single: false
+    name: "Brecht",
+    age: 23,
+    gender: "Male",
+    favorite_food: "Chicken Curry",
+    favorite_series: ["Chernobyl","Mad Men", "Black Mirror"],
+    favorite_movies: ["Tora Tora Tora", "Black Cat, White Cat"],
+    single: false
 };
 
 // Section Two 
@@ -23,23 +23,85 @@ var myFather = new Person("Peter", "Nulens", 57, "Brown");
 var myMother = new Person("Guadelupe", "Acedo", 52, "Grey");
 var myCoach = new Person("Shutsen", " ", 33, "Brown");
 
-let personalButton = document.getElementById("personal-button");
-let familyButton = document.getElementById("family-button");
+const personalButton = document.getElementById("personal-button");
+const familyButton = document.getElementById("family-button");
+const displayScreen = document.getElementById("screen");
+const personalScreenContent = document.getElementById("personal-content");
+const familyScreenContent = document.getElementById("family-content");
+var createdFamilyButtons = false;
 
 personalButton.addEventListener("click", function () {
-    document.getElementById("screen").innerHTML = "Hi, I am " + aboutMe.Name + " and I am " + aboutMe.Age + " years old.";
+    personalScreenContent.innerHTML = `Hi, I am ${aboutMe.name}, and I am ${aboutMe.age} years old.`
 })
 
-familyButton.addEventListener("click", function getFamilyMembers(){
+
+familyButton.addEventListener("click", function () {
+    if (createdFamilyButtons) {
+        return;
+    }
+
+    var div = document.createElement("div");
+    div.setAttribute("class","family-buttons");
+
+    var fatherButton = document.createElement("button");
+    var motherButton = document.createElement("button");
+    var coachButton = document.createElement("button");
+    
+    fatherButton.addEventListener("click", function () {
+        return showContent(myFather, 'father');
+    })
+    motherButton.addEventListener("click", function () {
+        return showContent(myMother, 'mother');
+    })
+    coachButton.addEventListener("click", function () {
+        return showContent(myCoach, 'coach');
+    })
+    
+    const fatherText = document.createTextNode("Father info");
+    const motherText = document.createTextNode("Mother info");
+    const coachText = document.createTextNode("Coach info");
+
+    fatherButton.appendChild(fatherText);
+    motherButton.appendChild(motherText);
+    coachButton.appendChild(coachText);
+
+    fatherButton.setAttribute("id","father-button");
+    motherButton.setAttribute("id","mother-button");
+    coachButton.setAttribute("id","coach-button");
+
+    div.appendChild(fatherButton);
+    div.appendChild(motherButton);
+    div.appendChild(coachButton);
+
+    displayScreen.insertBefore(div, displayScreen.childNodes[0]);
+
+    createdFamilyButtons ;
 })
+
+/**
+ * person = Object
+ * relation = String
+ */
+function showContent(person, relation) {
+    console.log(person, relation)
+    if (familyScreenContent.childNodes.length > 0) {
+        familyScreenContent.removeChild(familyScreenContent.childNodes[0]);
+    }
+    var p = document.createElement("p");
+    var content = document.createTextNode(`My ${relation}'s full name is ${person.firstName} ${person.lastName} and is ${person.age} years old`);
+
+    p.appendChild(content);
+    familyScreenContent.appendChild(p);
+
+    familyScreenContent.style.display = "block";
+}
 
 // Make BUTTONS toggle 
 function houdini () {
-    var x = document.getElementById("screen");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+    if (personalScreenContent.style.display === "none") {
+      personalScreenContent.style.display = "block";
     } else {
-      x.style.display = "none";
+      personalScreenContent.style.display = "none";
     }
 }
 
@@ -57,24 +119,6 @@ function buttonToggle(button) {
     }
 }
 
-function getFamilyMembers () {
-var div = document.createElement("div");
-var fatherButton = document.createElement("button");
-var motherButton = document.createElement("button");
-var coachButton = document.createElement("button");
-
-div.setAttribute("class","family-buttons");
-fatherButton.setAttribute("id","father-button");
-motherButton.setAttribute("id","mother-button");
-coachButton.setAttribute("id","coach-button");
-
-div.appendChild(fatherButton);
-div.appendChild(motherButton);
-div.appendChild(coachButton);
-
-return div;
-}
-          
 
 // var btn = document.createElement("BUTTON");   // Create a <button> element
 // btn.innerHTML = "CLICK ME";                   // Insert text
